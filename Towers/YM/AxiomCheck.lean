3df0099da0414026a87c9e3607ad4db14dd6e0d7
DAVID J FOX <davidjfox998@gmail.com>
2026-06-17 12:44:11 -0700

    mirror: Towers/YM/AxiomCheck.lean

---
+++ Towers/YM/AxiomCheck.lean
@@ -0,0 +1,6 @@
+import Towers.YM.L2Hilbert
+import Towers.YM.ShiftOperator
+import Towers.YM.NontrivialGap
+#print axioms TheoremaAureum.Towers.YM.OS.H
+#print axioms TheoremaAureum.Towers.YM.OS.norm_shift_apply
+#print axioms TheoremaAureum.Towers.YM.OS.nontrivial_gap
